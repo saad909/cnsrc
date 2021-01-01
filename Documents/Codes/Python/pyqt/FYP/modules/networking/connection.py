0bd2f89e3b1433b4a575ddafa4f35a48dab5bc4c
Abuhurraira Saad <abuhurraira.saad909@gmail.com>
2021-01-01 17:57:55 +0500

    show commands done against device and groups but running config and starup config not running

--- Documents/Codes/Python/pyqt/FYP/modules/networking/connection.py
+++ Documents/Codes/Python/pyqt/FYP/modules/networking/connection.py
@@ -1,73 +1,84 @@
 from PyQt5.QtWidgets import *
+
+import os
+import yaml
+from yaml import safe_load
 from pprint import pprint
-from netmiko import ConnectHandler,NetMikoTimeoutException
+
+from jinja2 import Environment, FileSystemLoader
+# from bracket_expansion import *
+from netmiko import Netmiko, file_transfer,ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from netmiko.ssh_exception import NetMikoAuthenticationException
+
+
+templates_path = os.path.join('hosts','templates')
+ENV = Environment(loader=FileSystemLoader(templates_path))
+
+
 class connection(QDialog):
 
     # craete the connection handler and return
-    def create_handler(device):
+    def create_handler(self,device):
+        hostname = device['hostname']
         device = device['data']
         try:
-            net_conn =  connecthandler(**device)
+            net_conn =  ConnectHandler(**device)
             return net_conn
-        except netmikotimeoutexception:
+        except NetMikoTimeoutException:
             print("devices in not reachable")
-            QMessageBox.information(self,"Note","devices in not reachable")
-        except netmikoauthenticationexception:
+            QMessageBox.information(self,"Note",f"{hostname} in not reachable")
+            return False
+        except NetMikoAuthenticationException:
             print("authentication failure")
-            QMessageBox.information(self,"Note","authentication failure")
-        except netmikosshexception:
+            QMessageBox.information(self,"Note",f"For {hostname} authentication Failed")
+            return False
+        except Exception as error:
             print("make sure ssh is enabled on device")
-            QMessageBox.information(self,"Note","make sure ssh is enabled on device")
+            QMessageBox.information(self,"Note",f"Make sure ssh is enabled on {hostname}")
+            print(error)
 
-    def write_commad_output(command_output):
-        with open('logs.txt', 'a') as filehandle:
-                filehandle.writelines("%s\n" % line for line in command_output)
+            return False
 
-    def write_message(message):
-        with open("logs.txt",'a') as handler:
-            handler.write(message)
+    # def write_commad_output(command_output):
+    #     with open('logs.txt', 'a') as filehandle:
+    #             filehandle.writelines("%s\n" % line for line in command_output)
 
-    def show_commands():
-        devices = read_yaml_file()
-        print(f"================= Total = {len(devices)} =================")
-        pprint(devices)
-
-        for device in devices:
-            message = f"-------------------- {device['hostname']}--------------------\n"
-            print(message)
-
-            write_message(message)
-            net_conn = create_handler(device['data'])
-            if net_conn:
-                output = net_conn.send_command("show ip int br",use_textfsm=True)
-                write_to_file(output)
-
-                print(f"ALL interfaces")
-                print(f"Total Interfaces = {len(output)}")
-
-                for interface in output:
-                    print(interface['intf'])
+    # def write_message(message):
+    #     with open("logs.txt",'a') as handler:
+    #         handler.write(message)
 
 
-                print(f"ALL up interfaces are")
+    # generate the configuration template
 
-                up_count = 0
-                for interface in output:
-                    if interface['status'] == 'up' and interface['proto'] == 'up':
-                        print(interface['intf'])
-                        up_count += 1
+    # 1. get the values from yaml file
+    # 2. feed those valus to jinj2 template
+    def gen(self,filename):
 
-                print(f"Total up = {up_count}")
+        print('-------------------')
+        print('')
+        var_file_path = os.path.join('hosts', 'vars', filename + '.yaml')
+        with open(var_file_path) as _:
+            var_file = safe_load(_)
+            print(var_file)
+            template = ENV.get_template(filename + ".j2")
+            print(template.render(config=var_file))
+
+    # use this template for those commands which dont require any arguments
+    def gen_false(self,filename):
+        filename = os.path.join('hosts', 'templates', filename + '.j2')
+        # filename  = ENV.get_template(filename + '.j2')
+        print(filename)
+        with open(filename,'r') as handler:
+            command = handler.read()
+        print(command)
+        return command
+
+        
+        
+
+            
 
 
-                print(f"ALL down interfaces are")
-
-                down_count = 0
-                for interface in output:
-                    if interface['status'] == 'down' or interface['proto'] == 'down':
-                        print(interface['intf'])
-                        down_count += 1
-
-                print(f"Total down  = {down_count}")
 
 
--- Documents/Codes/Python/pyqt/FYP/modules/networking/show_commands.py
+++ Documents/Codes/Python/pyqt/FYP/modules/networking/show_commands.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+import os
+from pprint import pprint
 
 class show_commands(QDialog):
     def run_show_command(self):
@@ -10,61 +12,108 @@ class show_commands(QDialog):
         # if group is selected
         if device_selection: 
             device_name = self.cb_bt_all_devices.currentText()
-            command_index = self.cb_bt_all_commands.currentIndex()
-            print('')
+            command_text= self.cb_bt_all_commands.currentText()
+            self.execute_show_command_against_single_device(device_name,command_text)
+
         # if single device is selected
         else:
-            group_name = self.cb_bt_all_groups.currentText()
-            command_index = self.cb_bt_all_commands.currentIndex()
 
-        # if device_name and group_name:
-        #     QMessageBox.information(self,"Note","Single device will be prefered")
-        # # Only device is selected
-        # elif device_name and not (group_name):
-        #     pass
+            group_name = self.cb_bt_all_groups.currentText()
+            command_text = self.cb_bt_all_commands.currentText()
+            self.execute_show_command_against_group(group_name,command_text)
+
+
+
+
+    def get_device(self,hostname):
+        devices = self.convert_host_file_into_list()
+        for device in devices:
+            if device['hostname'] == hostname:
+                return device
+
+    # execute show command agianst a single device
+    def execute_show_command(self,net_conn,filename):
+        filename = os.path.join('show',filename)
+        command = self.gen_false(filename)
+        output = net_conn.send_command(command)
+        print(output)
+        self.te_bt_command_output.setPlainText(str(output))
+
+
+    def execute_show_command_against_single_device(self, device_name,command_text):
+        print(f"Device name is {device_name}")
+        print(f"command to execute is {command_text}")
+
+        device = self.get_device(device_name)
+        net_conn = self.create_handler(device)
+
+        self.te_bt_command_output.clear()
+        if net_conn:
+            # show clock
+            if command_text == "clock":
+                self.execute_show_command(net_conn,'show_clock')
+                return
+
+            #show startup config
+            elif command_text == "startup config":
+                self.execute_show_command(net_conn,'show_start')
+                return
+
+            #show running config
+            elif command_text == "running config":
+                self.execute_show_command(net_conn,'show_run')
+                return
+            # sh interfaces information
+            elif command_text == "all interfaces configurations":
+                self.execute_show_command(net_conn,'show_interface_brief')
+                return
+            
         
 
-        # devices = read_yaml_file()
-        # print(f"================= Total = {len(devices)} =================")
-        # pprint(devices)
-
-        # for device in devices:
-        #     message = f"-------------------- {device['hostname']}--------------------\n"
-        #     print(message)
-
-        #     write_message(message)
-        #     net_conn = create_handler(device['data'])
-        #     if net_conn:
-        #         output = net_conn.send_command("show ip int br",use_textfsm=True)
-        #         write_to_file(output)
-
-        #         print(f"ALL interfaces")
-        #         print(f"Total Interfaces = {len(output)}")
-
-        #         for interface in output:
-        #             print(interface['intf'])
 
 
-        #         print(f"ALL up interfaces are")
-
-        #         up_count = 0
-        #         for interface in output:
-        #             if interface['status'] == 'up' and interface['proto'] == 'up':
-        #                 print(interface['intf'])
-        #                 up_count += 1
-
-        #         print(f"Total up = {up_count}")
 
 
-        #         print(f"ALL down interfaces are")
 
-        #         down_count = 0
-        #         for interface in output:
-        #             if interface['status'] == 'down' or interface['proto'] == 'down':
-        #                 print(interface['intf'])
-        #                 down_count += 1
+    # execute show command agianst a group
+    def execute_show_command_against_group(self, group_name, command_text):
+        print(f"Group name is {group_name}")
+        print(f"command to execute is {command_text}")
+        group_devices = list()
+        all_devices = self.convert_host_file_into_list()
 
-        #         print(f"Total down  = {down_count}")
+        for device in all_devices:
+            if group_name in device['groups']:
+                group_devices.append(device)
+        pprint(group_devices)
+        print(f"total devices = {len(group_devices)}")
+
+
+        self.te_bt_command_output.clear()
+        for device in group_devices:
+            net_conn = self.create_handler(device)
+            if net_conn:
+                print(f"\n================== {device['hostname']} ==================\n")
+                self.te_bt_command_output.setPlainText(f"\n================== {device['hostname']} ==================\n")
+                # show clock
+                if command_text == "clock":
+                    self.execute_show_command(net_conn,'show_clock')
+
+                #show startup config
+                elif command_text == "startup config":
+                    print("Startup config")
+                    self.execute_show_command(net_conn,'show_start')
+
+                #show running config
+                elif command_text == "running config":
+                    print("Running config")
+                    self.execute_show_command(net_conn,'show_run')
+                # sh interfaces information
+                elif command_text == "all interfaces configurations":
+                    self.execute_show_command(net_conn,'show_interface_brief')
+
+                elif command_text == "version":
+                    self.execute_show_command(net_conn,'show_version')
 
 
     def disable_box(self,checking_box,box_to_disable): 
