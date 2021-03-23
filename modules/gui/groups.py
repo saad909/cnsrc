6c795232d6c165cddc153ad9fda9c0d3afdf2337
Abuhurraira Saad <abuhurraira.saad909@gmail.com>
2021-03-23 14:01:49 +0500

    group addition section, listwidget clear selection and groupname successfull

--- modules/gui/groups.py
+++ modules/gui/groups.py
@@ -9,7 +9,9 @@ import os
 class groups(QDialog):
     def add_devices_for_group_selection(self):
         devices = self.get_all_devices_hostname()
+        # devices.sort()
         self.g_add_group_members.addItems(devices)
+        self.g_add_group_members.setSortingEnabled(True)
 
     def add_group(self):
         group_name = self.g_add_groupname.text()
@@ -27,7 +29,6 @@ class groups(QDialog):
                         device["groups"].append(group_name)
                         break
             # ask for confirmation
-
             selection = QMessageBox.question(
                 self,
                 "Alert",
@@ -42,9 +43,16 @@ class groups(QDialog):
             else:
                 QMessageBox.information(self, "Note", "Group did not added")
                 self.statusBar().showMessage("Failed")
+            # clear selections
+
+            self.g_add_groupname.setText("")
+            self.g_add_group_members.selectionModel().clear()
+            self.g_add_groupname.setFocus()
 
             # Need to add
-            # 1. add or remove a device to a certain group from devices section
-            # 2. show all groups
-            # 3. remove a group
-            # 4. edit a group
+            # 1. check for groupName duplication, already membership while editing
+            # 2. add or remove a device to a certain group from devices section
+            # 3. show all groups
+            # 4. remove a group
+            # 5. edit a group
+            # reflect data changes
--- modules/gui/inventory_mgmt_func.py
+++ modules/gui/inventory_mgmt_func.py
@@ -6,15 +6,14 @@ from pprint import pprint
 import os
 
 
-
 class inventory_mgmt_func(QDialog):
 
     # check hostfile
     # 1. check whether the hosts dir exists
-    #1a. if does not  exists create it
+    # 1a. if does not  exists create it
     # 2. if exist: check for invtory file
-        # 2a. if inventory file exists -> return True
-        #2b. else create an empty file
+    # 2a. if inventory file exists -> return True
+    # 2b. else create an empty file
     def check_for_host_file(self):
         # check the directory
         dir_exists = os.path.isdir("hosts")
@@ -23,13 +22,15 @@ class inventory_mgmt_func(QDialog):
         else:
             os.mkdir("hosts")
             # creating a dummy file
-            hostname = ip_address= username=password= secret =  "dummy"
-            device_type_index  = 0
-            device = self.create_dictionary( hostname , ip_address, username,password, secret ,device_type_index )
+            hostname = ip_address = username = password = secret = "dummy"
+            device_type_index = 0
+            device = self.create_dictionary(
+                hostname, ip_address, username, password, secret, device_type_index
+            )
             devices_list = list()
             devices_list.append(device)
-            f = open(host_file,"w+")
-            yaml.dump(devices_list,f, allow_unicode = True)
+            f = open(host_file, "w+")
+            yaml.dump(devices_list, f, allow_unicode=True)
             return False
 
         # check for file existance
@@ -38,51 +39,42 @@ class inventory_mgmt_func(QDialog):
         if not file_exists:
 
             # creating a dummy file
-            hostname = ip_address= username=password= secret =  "dummy"
-            device_type_index  = 0
-            device = self.create_dictionary( hostname , ip_address, username,password, secret ,device_type_index )
+            hostname = ip_address = username = password = secret = "dummy"
+            device_type_index = 0
+            device = self.create_dictionary(
+                hostname, ip_address, username, password, secret, device_type_index
+            )
             devices_list = list()
             devices_list.append(device)
-            f = open(host_file,"w+")
-            yaml.dump(devices_list,f, allow_unicode = True)
+            f = open(host_file, "w+")
+            yaml.dump(devices_list, f, allow_unicode=True)
             return False
-
 
         # file exists
         else:
             return True
 
-
-
-
-        
-
-
-
-
     # read the yaml file and returns a list
-    def read_yaml_file(self,yaml_file):
+    def read_yaml_file(self, yaml_file):
         devices = None
-        with open(yaml_file,'r') as handler:
+        with open(yaml_file, "r") as handler:
             devices = safe_load(handler)
         return devices
-
-
 
     # read the yaml file and returns a list
     def convert_host_file_into_list(self):
         host_file = self.get_host_file_path()
         devices = None
-        with open(host_file,'r') as handler:
+        with open(host_file, "r") as handler:
             devices = safe_load(handler)
         return devices
-
-
 
     # input = device parameters
     # output = dictionary created from those parameter
     # output['data'] -> contains the information for actual connection
-    def create_dictionary( self,hostname , ip_address, username,password, secret ,device_type_index ):
+    def create_dictionary(
+        self, hostname, ip_address, username, password, secret, device_type_index
+    ):
         device_type = None
         if device_type_index == 1:
             device_type = "router"
@@ -92,36 +84,32 @@ class inventory_mgmt_func(QDialog):
             device_type = "dummy"
 
         return {
-                'hostname': hostname,
+            "hostname": hostname,
+            "data": {
+                "device_type": "cisco_ios",
+                "host": ip_address,
+                "username": username,
+                "password": password,
+                "secret": secret,
+            },
+            "type": device_type,
+            "groups": [],
+        }
 
-                "data": {
-
-                    "device_type" : 'cisco_ios',
-                    "host": ip_address,
-                    'username': username,
-                    'password': password,
-                    'secret': secret
-
-                },
-                'type': device_type,
-                'groups': []
-            }
-        
-
-     
     # get os independent inventory file path
     def get_host_file_path(self):
-        return os.path.join("hosts","inventory.yaml")
+        return os.path.join("hosts", "inventory.yaml")
 
-
-    #1. check for duplication of hostname in the database
-    #2.return True in case of duplication -> True,False
-    #3. True -> duplication occurs
-    def check_hostname_duplication(self,all_hosts,host): 
+    # 1. check for duplication of hostname in the database
+    # 2.return True in case of duplication -> True,False
+    # 3. True -> duplication occurs
+    def check_hostname_duplication(self, all_hosts, host):
         for device in all_hosts:
-            if device["hostname"] == host['hostname']:
+            if device["hostname"] == host["hostname"]:
                 print("Host name duplication occured")
-                QMessageBox.information(self,"Warning","Host name duplication occured")
+                QMessageBox.information(
+                    self, "Warning", "Host name duplication occured"
+                )
 
                 self.d_add_hostname.setFocus()
                 self.highlight_border(self.d_add_hostname)
@@ -129,14 +117,18 @@ class inventory_mgmt_func(QDialog):
         self.highlight_border_false(self.d_add_hostname)
         return False
 
-    #1. check for duplication of ip address in the database
-    #2.return True in case of duplication -> True,False
-    #3. True -> duplication occurs
-    def check_ip_duplication(self,all_hosts,host): # return True in case of duplication
+    # 1. check for duplication of ip address in the database
+    # 2.return True in case of duplication -> True,False
+    # 3. True -> duplication occurs
+    def check_ip_duplication(
+        self, all_hosts, host
+    ):  # return True in case of duplication
         for device in all_hosts:
-            if device['data']["host"] == host['data']['host']:
+            if device["data"]["host"] == host["data"]["host"]:
                 print(f"{device['hostname']} has the same ip address ")
-                QMessageBox.information(self,"Warning",f"{device['hostname']} has the same ip address ")
+                QMessageBox.information(
+                    self, "Warning", f"{device['hostname']} has the same ip address "
+                )
 
                 self.d_add_ip_address.setFocus()
                 self.highlight_border(self.d_add_ip_address)
@@ -145,16 +137,15 @@ class inventory_mgmt_func(QDialog):
         self.highlight_border_false(self.d_add_ip_address)
         return False
 
-
     # write devices to the inventory file
     # takes a list as input
-    def write_inventory(self,all_devices_list):
+    def write_inventory(self, all_devices_list):
         host_file = self.get_host_file_path()
-        f = open(host_file,'w+')
-        yaml.dump(all_devices_list,f,allow_unicode=True)
+        f = open(host_file, "w+")
+        yaml.dump(all_devices_list, f, allow_unicode=True)
 
     # how the device is added
-    def add_host_into_inventory(self,device):
+    def add_host_into_inventory(self, device):
         # checking for empty inventory
         host_file_not_empty = self.check_for_host_file()
         all_devices_list = None
@@ -165,44 +156,38 @@ class inventory_mgmt_func(QDialog):
             all_devices_list = self.convert_host_file_into_list()
 
             # check  whether it contains the dummy device
-                
 
             # remove the dummy file
             if len(all_devices_list) == 1:
-                if all_devices_list[0]['hostname'] == 'dummy':
+                if all_devices_list[0]["hostname"] == "dummy":
                     all_devices_list.pop(0)
 
-
-
             # check whether the ip address exists before
-            hostname_duplication = self.check_hostname_duplication(all_devices_list,device)
+            hostname_duplication = self.check_hostname_duplication(
+                all_devices_list, device
+            )
             if hostname_duplication:
                 return False
             # check whether the ip address exists before
-            ip_duplication = self.check_ip_duplication(all_devices_list,device)
+            ip_duplication = self.check_ip_duplication(all_devices_list, device)
             if ip_duplication:
                 return False
 
-            print('=================== Already Present Devices ==================')
+            print("=================== Already Present Devices ==================")
             pprint(all_devices_list)
-
-
 
             # get the hosts from the user and create its dictionary
             all_devices_list.append(device)
 
-            
-            print('-----------------------ALL Devices in host file --------------------------------')
+            print(
+                "-----------------------ALL Devices in host file --------------------------------"
+            )
 
             pprint(all_devices_list)
 
-
-            #adding the device to the group based on its type
-            groupname = device['type']
-            device['groups'].append(groupname)
-
+            # adding the device to the group based on its type
+            groupname = device["type"]
+            device["groups"].append(groupname)
 
             self.write_inventory(all_devices_list)
             return True
-
-
