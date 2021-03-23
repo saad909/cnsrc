8171c998c22740562c4583452a5ddb780305d5c3
Abuhurraira Saad <abuhurraira.saad909@gmail.com>
2021-03-23 16:37:19 +0500

    custom group.yaml file created but need to check for group duplication

--- modules/gui/groups.py
+++ modules/gui/groups.py
@@ -13,21 +13,84 @@ class groups(QDialog):
         self.g_add_group_members.addItems(devices)
         self.g_add_group_members.setSortingEnabled(True)
 
+    def get_group_file_path(self):
+        return os.path.join("hosts", "groups.yaml")
+
+    def create_dictionary_for_group(self, group_name, group_members):
+        return {"group_name": group_name, "group_members": group_members}
+
+    def check_for_group_file(self):
+        # check the directory
+        dir_exists = os.path.isdir("hosts")
+        group_file = self.get_group_file_path()
+        if dir_exists:
+            print("Folder exists")
+        else:
+            os.mkdir("hosts")
+            # creating a dummy file
+            group_name = "dummy"
+            group_members = list()
+            demo_group = self.create_dictionary_for_group(group_name, group_members)
+            groups = list()
+            groups.append(demo_group)
+
+            f = open(group_file, "w")
+            yaml.dump(groups, f, allow_unicode=True)
+            return False
+
+        # check for file existance
+        file_exists = os.path.isfile(group_file)
+        if not file_exists:
+            print("group file does not exists")
+
+            group_name = "dummy"
+            group_members = list()
+            demo_group = self.create_dictionary_for_group(group_name, group_members)
+            groups = list()
+            groups.append(demo_group)
+
+            f = open(group_file, "w")
+            yaml.dump(groups, f, allow_unicode=True)
+            return False
+
+        # file exists
+        else:
+            print("group file exists")
+            return True
+
+    def convert_group_file_into_list(self):
+        group_file = self.get_group_file_path()
+        groups = None
+        with open(group_file, "r") as handler:
+            groups = safe_load(handler)
+        return groups
+
+    def add_group_into_file(self, group_name, group_members):
+        self.check_for_group_file()
+        groups = self.convert_group_file_into_list()
+        group = self.create_dictionary_for_group(group_name, group_members)
+        if len(groups) == 1 and groups[0]["group_name"] == "demo":
+            groups.pop(0)
+            groups.append(group)
+        else:
+            groups.append(group)
+        print(group)
+        pprint(groups)
+        return groups
+
+    def write_group_file(self, groups):
+        group_file = self.get_group_file_path()
+        f = open(group_file, "w+")
+        yaml.dump(groups, f, allow_unicode=True)
+
     def add_group(self):
         group_name = self.g_add_groupname.text()
         group_members = list()
         for device in self.g_add_group_members.selectedItems():
             group_members.append(device.text())
-        pprint(group_members)
         if group_name and group_members:
-            devices = self.convert_host_file_into_list()
+            groups = self.add_group_into_file(group_name, group_members)
 
-            # add the selected users to the group
-            for member in group_members:
-                for device in devices:
-                    if device["hostname"] == member:
-                        device["groups"].append(group_name)
-                        break
             # ask for confirmation
             selection = QMessageBox.question(
                 self,
@@ -37,9 +100,10 @@ class groups(QDialog):
                 QMessageBox.No,
             )
             if selection == QMessageBox.Yes:
-                self.write_inventory(devices)
+                self.write_group_file(groups)
                 QMessageBox.information(self, "Note", "Group added successfully")
                 self.statusBar().showMessage("Succesfuly")
+
             else:
                 QMessageBox.information(self, "Note", "Group did not added")
                 self.statusBar().showMessage("Failed")
@@ -49,10 +113,10 @@ class groups(QDialog):
             self.g_add_group_members.selectionModel().clear()
             self.g_add_groupname.setFocus()
 
-            # Need to add
-            # 1. check for groupName duplication, already membership while editing
-            # 2. add or remove a device to a certain group from devices section
-            # 3. show all groups
-            # 4. remove a group
-            # 5. edit a group
-            # reflect data changes
+        # Need to add
+        # 1. check for groupName duplication, already membership while editing
+        # 2. add or remove a device to a certain group from devices section
+        # 3. show all groups
+        # 4. remove a group
+        # 5. edit a group
+        # reflect data changes
