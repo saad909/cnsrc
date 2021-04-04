ef888f8287e2468ce0eeaf7a8ffa33ec6ada9f71
Abuhurraira Saad <abuhurraira.saad909@gmail.com>
2021-04-04 14:40:37 +0500

    fixed - decrypt password while sending the commands to the devices

--- modules/networking/connection.py
+++ modules/networking/connection.py
@@ -6,63 +6,68 @@ from yaml import safe_load
 from pprint import pprint
 
 from jinja2 import Environment, FileSystemLoader
+
 # from bracket_expansion import *
-from netmiko import Netmiko, file_transfer,ConnectHandler
+from netmiko import Netmiko, file_transfer, ConnectHandler
 from netmiko.ssh_exception import NetMikoTimeoutException
 from netmiko.ssh_exception import AuthenticationException
 from paramiko.ssh_exception import SSHException
 
 
-templates_path = os.path.join('hosts','templates')
+templates_path = os.path.join("hosts", "templates")
 ENV = Environment(loader=FileSystemLoader(templates_path))
 
 
 class connection(QDialog):
 
     # craete the connection handler and return
-    def create_handler(self,device):
-        hostname = device['hostname']
-        device = device['data']
+    def create_handler(self, device):
+        # decrypt deivce passwords
+        device["data"]["password"] = self.decrypt_password(device["data"]["password"])
+        device["data"]["secret"] = self.decrypt_password(device["data"]["secret"])
+
+        hostname = device["hostname"]
+        device = device["data"]
         try:
-            net_conn =  ConnectHandler(**device)
+            net_conn = ConnectHandler(**device)
             return net_conn
         except NetMikoTimeoutException:
             print("devices in not reachable")
-            QMessageBox.information(self,"Note",f"{hostname} in not reachable")
+            QMessageBox.information(self, "Note", f"{hostname} in not reachable")
             return False
         except AuthenticationException:
             print("authentication failure")
-            QMessageBox.information(self,"Note",f"For {hostname} authentication Failed")
+            QMessageBox.information(
+                self, "Note", f"For {hostname} authentication Failed"
+            )
             return False
 
         except SSHException:
             print("SSH error. Make sure ssh is enabled on device")
-            QMessageBox.information(self,"Note",f"SSH error. Make sure ssh is enabled on {hostname}")
+            QMessageBox.information(
+                self, "Note", f"SSH error. Make sure ssh is enabled on {hostname}"
+            )
             print(str(error))
         except Exception as error:
             print(str(error))
-            QMessageBox.information(self,"Note",str(error))
+            QMessageBox.information(self, "Note", str(error))
             return False
-
-
 
     # generate the configuration template
 
     # 1. get the values from yaml file
     # 2. feed those valus to jinj2 template
-    def gen(self,configs_dictionary, filename):
-        template_path = os.path.join(filename + '.j2')
+    def gen(self, configs_dictionary, filename):
+        template_path = os.path.join(filename + ".j2")
         template = ENV.get_template(template_path)
         return template.render(config=configs_dictionary)
 
-
-
     # use this template for those commands which dont require any arguments
-    def gen_false(self,filename):
-        filename = os.path.join('hosts', 'templates', filename + '.j2')
+    def gen_false(self, filename):
+        filename = os.path.join("hosts", "templates", filename + ".j2")
         # filename  = ENV.get_template(filename + '.j2')
         print(filename)
-        with open(filename,'r') as handler:
+        with open(filename, "r") as handler:
             command = handler.read()
         print(command)
         return command
